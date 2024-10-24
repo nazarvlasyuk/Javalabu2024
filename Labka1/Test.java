8d3b5fae8ad5dbd6dbdc6122ce2d5ae2a48cd367
nazarvlasyuk <163637021+nazarvlasyuk@users.noreply.github.com>
2024-10-24 08:55:38 +0300

    Add files via upload

---
+++ Labka1/Test.java
@@ -0,0 +1,10 @@
+package Labka1;
+
+public class Test {
+    public static void main(String[] args) {
+        double degrees = 30;
+        double radians = Math.toRadians(degrees);
+        double sinValue= Math.sin(radians);
+        System.out.println("сінус кута 30 градусів: " + sinValue);
+    }
+}
